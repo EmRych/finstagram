b95f8ce5ac2a8fbedbaaca3797d1febe6e93b9f5
EmRych <er.rychel@gmail.com>
2022-02-01 23:28:55 +0000

    W3D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,3 @@
-#get '/' do
-#    File.read(File.join('app/views', 'index.html'))
-#end
 
 def humanized_time_ago(time_ago_in_minutes)
     if time_ago_in_minutes >= 60
@@ -11,7 +8,7 @@ def humanized_time_ago(time_ago_in_minutes)
   end
   
   get '/' do
-    finstagram_post_shark = {
+    @finstagram_post_shark = {
       username: "sharky_j",
       avatar_url: "http://naserca.com/images/sharky_j.jpg",
       photo_url: "http://naserca.com/images/shark.jpg",
@@ -24,7 +21,7 @@ def humanized_time_ago(time_ago_in_minutes)
       }]
     }
   
-    finstagram_post_whale = {
+    @finstagram_post_whale = {
       username: "kirk_whalum",
       avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
       photo_url: "http://naserca.com/images/whale.jpg",
@@ -37,7 +34,7 @@ def humanized_time_ago(time_ago_in_minutes)
       }]
     }
   
-    finstagram_post_marlin = {
+    @finstagram_post_marlin = {
       username: "marlin_peppa",
       avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
       photo_url: "http://naserca.com/images/marlin.jpg",
@@ -49,5 +46,9 @@ def humanized_time_ago(time_ago_in_minutes)
         text: "lunchtime! ;)"
       }]
     }
-    [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+    [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
+
+    @finstagram_posts=[@finstagram_post_shark,@finstagram_post_whale,@finstagram_post_marlin]
+
+    erb(:index)
   end
