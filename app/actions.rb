c69aae44e446e8bf4b680fc0c686e50ccdbfdd5c
EmRych <er.rychel@gmail.com>
2022-02-10 01:41:58 +0000

    W4D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,22 @@
 get '/' do
-  @finstagram_posts = FinstagramPost.order(created_at::desc)
+  @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
+end
+
+get '/signup' do
+  @user = User.new
+  erb(:signup)
+end
+
+post '/signup' do
+
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+  user.save
+
+  escape_html user.inspect
 end
